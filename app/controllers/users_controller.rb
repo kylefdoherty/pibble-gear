11b5e2db12476b4d073ea24c383da03d2b8a910e
Kyle Doherty <kyledoherty22@gmail.com>
2015-09-07 23:47:04 +0100

    Add user update and create pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
+  def new
+    profile = Profile.new
+    @user = User.new(profile: profile)
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    binding.pry
+
+    if @user.save
+      flash[:notice] = "user created"
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong and user wasn't created."
+      redirect_to :back
+    end
+  end
+
   def show
   end
 
@@ -27,6 +46,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, profile_attributes: [:name, :bio])
+    params.require(:user).permit(:email, :password, :password_confirmation, profile_attributes: [:name, :bio])
   end
 end
